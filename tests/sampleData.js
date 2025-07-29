6da8f3e6810e02202fb5a6a737a5c6d229c71962
Metehan Özcan <70481060+metehanozcan@users.noreply.github.com>
2025-07-29 15:39:30 +1000

    Add basic test for getBestPrices

---
+++ tests/sampleData.js
@@ -0,0 +1,68 @@
+module.exports = {
+  success: true,
+  data: {
+    bynogame: {
+      site: 'bynogame',
+      name: 'ByNoGame',
+      products: [],
+      status: 'success',
+      timestamp: '2025-07-29T05:22:30.843Z'
+    },
+    oyunfor: {
+      site: 'oyunfor',
+      name: 'OyunFor',
+      products: [
+        { server: 'ZERO', buyPrice: 2.85, sellPrice: 2.65, unit: '1M', originalName: 'Knight Online ZERO GB' },
+        { server: 'FELIS', buyPrice: 1.75, sellPrice: 1.6, unit: '1M', originalName: 'Knight Online FELIS GB' },
+        { server: 'AGARTHA', buyPrice: 2.75, sellPrice: 2.55, unit: '1M', originalName: 'Knight Online AGARTHA GB' },
+        { server: 'PANDORA', buyPrice: 2.8, sellPrice: 2.55, unit: '1M', originalName: 'Knight Online PANDORA GB' },
+        { server: 'DRYADS', buyPrice: 2.1, sellPrice: 1.9, unit: '1M', originalName: 'Knight Online DRYADS GB' },
+        { server: 'DESTAN', buyPrice: 3.75, sellPrice: 3.52, unit: '1M', originalName: 'Knight Online DESTAN GB' },
+        { server: 'OREADS', buyPrice: 3.1, sellPrice: 2.85, unit: '1M', originalName: 'Knight Online OREADS GB' }
+      ],
+      status: 'success',
+      timestamp: '2025-07-29T05:24:57.143Z'
+    },
+    kopazar: {
+      site: 'kopazar',
+      name: 'Kopazar',
+      products: [
+        { server: 'Zero', buyPrice: 2.84, sellPrice: 2.65, unit: '1M', originalName: 'Zero GbZero 10M Teslimat Server 6 Moradon Sağ banka Nia' },
+        { server: 'Pandora', buyPrice: 2.8, sellPrice: 2.55, unit: '1M', originalName: 'Pandora GbPandora 10M Teslimat Server 2 Moradon Sağ banka Nia' },
+        { server: 'Agartha', buyPrice: 2.7, sellPrice: 2.5, unit: '1M', originalName: 'Agartha GbAgartha 10M Teslimat Server 2 Moradon Sağ banka Nia' },
+        { server: 'Felis', buyPrice: 1.8, sellPrice: 1.65, unit: '1M', originalName: 'Felis GbFelis 10M Teslimat Server 2 Moradon Sağ banka Nia' },
+        { server: 'Destan', buyPrice: 3.74, sellPrice: 3.5, unit: '1M', originalName: 'Destan GbDestan 10M Teslimat SERVER 3 Maradon Sağ Banka Nia' },
+        { server: 'Minark', buyPrice: 2.6, sellPrice: 2.4, unit: '1M', originalName: 'Minark GbMinark 10M Teslimat SERVER 2 Maradon Sağ Banka Nia' },
+        { server: 'Dryads', buyPrice: 2.09, sellPrice: 1.85, unit: '1M', originalName: 'Dryads GbDryads 10M Teslimat SERVER 2 Maradon Sağ Banka Nia' },
+        { server: 'Oreads', buyPrice: 3.2, sellPrice: 2.9, unit: '1M', originalName: 'Oreads GbOreads 10M Teslimat SERVER 3 Maradon Sağ Banka Nia' }
+      ],
+      status: 'success',
+      timestamp: '2025-07-29T05:21:24.149Z'
+    },
+    yesilyurtgame: {
+      site: 'yesilyurtgame',
+      name: 'Yeşilyurt Game',
+      products: [],
+      status: 'success',
+      timestamp: '2025-07-29T05:21:19.753Z'
+    },
+    klasgame: {
+      site: 'klasgame',
+      name: 'KlasGame',
+      products: [],
+      status: 'success',
+      timestamp: '2025-07-29T05:21:18.767Z'
+    },
+    'yeşilyurtgame': {
+      site: 'yeşilyurtgame',
+      name: 'Yeşilyurt Game',
+      products: [],
+      status: 'error',
+      error: 'Overall timeout',
+      timestamp: '2025-07-29T05:28:46.000Z'
+    }
+  },
+  lastUpdate: '2025-07-29T05:28:46.107Z',
+  isUpdating: false,
+  timestamp: '2025-07-29T05:33:46.526Z'
+};
